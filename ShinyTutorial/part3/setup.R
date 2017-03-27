1b996adce7207a36db17f7f36e08ba9c034523d3
Michael Ding <mding@dyn172-30-81-165.wireless.uwo.pri>
2017-03-27 17:03:29 -0400

    Started ShinyTutorial Part3

---
+++ ShinyTutorial/part3/setup.R
@@ -0,0 +1,16 @@
+# Setup should be used as a config file for all packages, special constants and variables used 
+
+# Clears global environment to remove any possible conflicts in dependencies, etc.
+rm(list = ls())
+
+# Packages installed and used
+packagesInstalled <- c("shiny","easypackages") # use this to specify the packages to install
+install.packages(packagesInstalled)
+update.packages(packagesInstalled)
+library(easypackages) # use this to load + managed multiple packages easily
+packagesUsed <- c("shiny") # use this to specify the packages to load
+libraries(packagesUsed)
+
+# Special global constants
+
+# Special global variables
